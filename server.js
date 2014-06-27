f50e271d05bb517a5638112e884323bf794d49c0
李鑫(媒体内容业务线.202368) <micro-tech@foxmail.com>
2014-06-27 14:30:09 +0800

    Use ejs in development env

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 #!/bin/env node
 var express = require('express');
-var app = express();
-app.use(express.static('public'));
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var ejs = require('ejs');
+
+var app = module.exports = express();
+
+// Auto render templates
+var extension = 'html';
+var dir = 'public';
+app.set('views', '.');
+app.engine(extension, ejs.__express);
+app.use(function(req, res, next) {
+  var pathname = url.parse(req.url).pathname;
+  pathname = path.join(dir, pathname);
+
+  fs.lstat(pathname, function(err, stats) {
+    // Requests that match /dir will be interpreted as /dir/index
+    if(!err && stats.isDirectory()) {
+       pathname = path.join(pathname, 'index');
+    }
+    pathname += '.' + extension;
+
+    fs.lstat(pathname, function(err, stats) {
+      if(!err && stats.isFile()) {
+        res.render(pathname, {enviroment: 'development'});
+      } else {
+        next();
+      }
+    });
+  });
+});
+
+app.use(express.static(dir));
 app.use('/lib', express.static('bower_components'));
 process.env.PORT = process.env.PORT || 3000;
-app.listen(process.env.PORT, function () {
-  console.log('listen on port', process.env.PORT);
-});
+if (!module.parent) {
+  app.listen(process.env.PORT);
+  console.log('listening on port', process.env.PORT);
+}
